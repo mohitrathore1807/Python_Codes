276c3bc95616de1a599c366bec8e2f06c38c905f
Ayush05m <72142971+Ayush05m@users.noreply.github.com>
2020-09-30 21:31:02 +0530

    Update 3. Damru.py

--- 3. Damru.py
+++ 3. Damru.py
@@ -5,7 +5,7 @@
 
 
 # Program 3 :- Damru
-n = int(input())
+n = int(input('PLEASE ENTER THE SIZE : ))
 for i in range(n+1):
     for j in range(n+1):
         if j <= i and j >= (n-i) or j >= i and j <= (n-i) and j <= n:
