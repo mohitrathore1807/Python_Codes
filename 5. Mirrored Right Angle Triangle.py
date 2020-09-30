c3b59c75a22f6ed99477aaa668accd0b3f528921
Ayush05m <72142971+Ayush05m@users.noreply.github.com>
2020-09-30 21:26:14 +0530

    Update 5. Mirrored Right Angle Triangle.py

--- 5. Mirrored Right Angle Triangle.py
+++ 5. Mirrored Right Angle Triangle.py
@@ -5,12 +5,14 @@
 
 
 # Program 5 :- Mirrored Right Triangle Star Pattern
-n = int(input())
+n = int(input("PLEASE ENTER THE NUMBERS OF ROWS YOU WANT : "))
 for i in range(1, n+1):
     for j in range(n):
         if j >= (n-i) :
             print("* ", end = "")
         else:
+            print("PLEASE ENTER ANY DIGITS")
             print("", end = "  ")
     print("\r")
+print('Thank you')
 
